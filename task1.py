edb26f9519228d677fe720f368af4ec5c04a6d5d
Liam751 <55156691+Liam751@users.noreply.github.com>
2021-02-25 09:34:46 -0800

    work

--- task1.py
+++ task1.py
@@ -1,15 +1,23 @@
 #!python3
+#a = float(input("A number: "))
+#b = float(input(""))
+
 
 def sum(a,b):
     #inputs
     # a : float 
     # b : float
     # return value: returns the sum of the 2 numbers
+    c = a + b
+    return c
+    
 
 
 
 #this should return a value of 7
 x = sum(3,4)
+print(x)
 
 #this should return a value of 12.5
 y = sum(11,1.5)
+print(y)
