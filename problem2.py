3f74074e3a9c433acfcbf6f01d8603eff56a9fbb
Liam751 <55156691+Liam751@users.noreply.github.com>
2021-03-01 16:22:21 -0800

    work

--- problem2.py
+++ problem2.py
@@ -9,3 +9,9 @@ Note that the coordinates should be signed (positive or negative) floats
 """
 import math
 
+def distance( [x1,y1] , [x2,y2] ):
+    a = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return a
+
+x = distance( (2,4) , (6,3))
+print(x)
